7ff94b56e9df48f146f9cdb3a4de5a5b2ddb65d7
trickste <nishantprakashsaini285@gmail.com>
2020-07-15 23:36:39 +0530

    backup script

---
+++ backup_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+host_tag="attendance"
+key_path="~/.jenkins/ec2-linux-public-01.pem"
+service_name="attendance"
+host_ips=$(python dynamic-inventory.py $host_tag)
+`echo host_ips` wc -l
+for host_ip in "${host_ips[@]}"
+do
+        application_present=$(ssh -o "StrictHostKeyChecking no" -i $key_path ubuntu@$host_ip """if [ -d /usr/local/$service_name ]
+then
+        echo "true"
+else
+        echo "false"
+fi """)
+if [ $application_present == "true" ]
+then    
+        echo "true"
+        scp -i $key_path -r ubuntu@$host_ip:/usr/local/$service_name .
+else    
+        echo "false"
+fi
+done
