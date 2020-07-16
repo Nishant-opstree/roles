3f0542b0942c5f38cc97ddbca2ff0496b21d65f1
trickste <nishantprakashsaini285@gmail.com>
2020-07-16 08:43:18 +0530

    update

--- backup_script.sh
+++ backup_script.sh
@@ -3,14 +3,16 @@ host_tag=$1
 key_path=$2
 service_name=$3
 host_ips=($(python dynamic-inventory.py $host_tag))
+count=1
 for host_ip in "${host_ips[@]}"
 do
         application_present=`ssh -o "StrictHostKeyChecking no" -i $key_path ubuntu@$host_ip "if [ -d /usr/local/$service_name ] ; then echo "true"; else echo "false" ; fi "`
 if [ $application_present == "true" ]
 then    
         echo "true2"
-	mkdir attendance
-        scp -i $key_path --exclude='attendance' -r ubuntu@$host_ip:/usr/local/$service_name . > /dev/null 2>&1
+	mkdir attendance_"$count"
+        scp -i $key_path --exclude='attendance' -r ubuntu@$host_ip:/usr/local/$service_name ./attendance_"$count" > /dev/null 2>&1
+	count=$((count+1))
 else    
         echo "false2"
 fi
